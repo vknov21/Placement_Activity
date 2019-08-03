c260eec90f6cdffd1d3577a1c6d41803f72216c7
vknov21 <53618575+vknov21@users.noreply.github.com>
2019-08-04 00:21:27 +0530

    Update Smaller_Numbers.py

--- 1st/Smaller_Numbers.py
+++ 1st/Smaller_Numbers.py
@@ -1,10 +1,10 @@
 lst=[]
-size=int(input("Enter size of list : "))
-if(size<2):
+size=int(input("Enter size of list : "))	//Size of List
+if(size<2):					//Atleast two greater should be present
 	exit()
 for i in range(0,size):
 	ele=int(input("Element %d : " %(i+1)))
-	lst.append(ele)
-lst.remove(max(lst))
+	lst.append(ele)				//Taking input from user
+lst.remove(max(lst))				//Removing two max numbers
 lst.remove(max(lst))
 print(lst)
